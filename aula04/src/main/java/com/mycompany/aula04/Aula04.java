61b8f7d7d3ccc03c80412a886f5b21a9102865a2
Leonardo <leonardowaschburger28@gmail.com>
2025-08-25 19:40:32 -0300

    Primeiro commit da aplicação

---
+++ aula04/src/main/java/com/mycompany/aula04/Aula04.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aula04;
+
+/**
+ *
+ * @author 08294
+ */
+public class Aula04 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
